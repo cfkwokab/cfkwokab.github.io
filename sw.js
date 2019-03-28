0512e21bde1195c06b94f1ee3f7780722707b656
cfkwokab <48879610+cfkwokab@users.noreply.github.com>
2019-03-28 22:25:05 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,33 @@
-// 监听 service worker 的 install 事件
-this.addEventListener('install', function (event) {
-    // 如果监听到了 service worker 已经安装成功的话，就会调用 event.waitUntil 回调函数
-    event.waitUntil(
-        // 安装成功后操作 CacheStorage 缓存，使用之前需要先通过 caches.open() 打开对应缓存空间。
-        caches.open('my-test-cache-v1').then(function (cache) {
-            // 通过 cache 缓存对象的 addAll 方法添加 precache 缓存
-            return cache.addAll([
-                '/',
-                '/index.html'
-            ]);
-        })
-    );
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+  '/index.html',
+  '/nomore.png',
+  '/clown.png'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
