ae79f25a7471a7bc4f51cdd801c9908f937dc020
cfkwokab <48879610+cfkwokab@users.noreply.github.com>
2019-05-02 20:18:00 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,54 @@
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+  '/index.html',
+  '/nomore.png',
+  '/clown.png',
+  '/manifest.json',
+  '/favicon.ico',
+  '/sw.js',
+  '/jquery-3.3.1.min.js',
+  '/materialize.min.js',
+  '/materialize.min.css',
+  '/jquery-ui.min.js',
+  '/game.js',
+  '/font.css',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  '/iconbw.svg',
+  '/Splash.js',
+  '/regSW.js',
+  '/Over.js',
+  '/installbutton.js',
+  '/music.mp3',
+  '/time.js',
+  '/icon-bg.png',
+  '/icon.png',
+  '/6aez4K2oVqwIvtU2Hw.woff2',
+  '/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.woff2'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
