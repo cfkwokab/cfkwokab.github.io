1621080f188603aa562e8998601054c6fb72a3f7
cfkwokab <48879610+cfkwokab@users.noreply.github.com>
2019-05-05 18:38:03 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -48,7 +48,7 @@ self.addEventListener('fetch', function(event) {
         if (response) {
           return response;
         }
-        return fetch(event.request);
+        return fetch(event.request).then(function(response) {return response;});
       }
     )
   );
